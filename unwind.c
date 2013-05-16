2db6b157c6dba99047410cb7cffdab291b349f22
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-05-16 11:41:21 -0400

    Clean up some of unwind.c, add .gitignore

--- unwind.c
+++ unwind.c
@@ -32,8 +32,6 @@ static int NumberOfFluxes; // Q0
 static int DimensionOfBrane;
 
 static int integer_part(double x);
-//static double polylog(int n, double x);
-
 static double F_function(double chi);
 static double potential_function(double z);
 static double potential_derivative(double z);
@@ -83,25 +81,31 @@ void dXdt(double *X, double *dXdt)
   double g = X[2];
   double a = X[3];
   
-  double dVdz = potential_derivative(z); 
-  double chi  = atanh( sqrt(g*g - 1)/g );
-  double gs   = StringCoupling;
   int p       = DimensionOfBrane;
-  double dp   = pow(LengthOfSmallExtraDimensions, p-3); // also called Vwrap in da-code
+  double dVdz = potential_derivative(z); 
+  double chi  = atanh(sqrt(g*g - 1)/g);
+  double gs   = StringCoupling;
+  double dp   = pow(LengthOfSmallExtraDimensions, p-3);
   double ms   = StringMass;
-  double sig  = pow(ms, p+1) * dp / (gs * 0.5*pow(2*PI,(p-1)));
+  double sig  = pow(ms, p+1) * dp / (gs * 0.5*pow(2*PI, p-1));
   double M4   = FourDimensionalPlanckMass;
   double V    = potential_function(z);
+
+  double S1z = S1(z);
+  double S2z = S2(z);
   
   dXdt[0] = 1.0; // = t'
   dXdt[1] = sqrt(g*g - 1) / g; // = z'
-  dXdt[3] = a/(sqrt(3.0)*M4) * sqrt( V + sqrt(g*g -1)/(g*chi) * S1(z)/pow(a,3) + 2.0*sig*g );  // = a'
+  dXdt[3] = a/(sqrt(3.0)*M4) *
+    sqrt(V + sqrt(g*g - 1)/(g*chi) * S1z/pow(a,3) + 2.0*sig*g); // = a'
 
   double term1 = -3.0 * dXdt[3]/a * (g*g - 1)/g;
   double term2 = -0.5 * dVdz * sqrt(g*g - 1) / (g * sig);
-  double term3 = -0.5 * (g*g - 1)/(g*chi) * 1/( sig * pow(a,3) ) * S2(z);
-  double denom = 1.0 + 0.5/(g*chi) * ( 1/( g*sqrt(g*g-1) ) - 1/chi ) * S1(z)/( sig * pow(a,3) );
-  dXdt[2] = (term1 + term2 + term3) / denom;  // = g'  
+  double term3 = -0.5 * (g*g - 1)/(g*chi) * 1/( sig * pow(a, 3)) * S2z;
+  double denom = 1.0 + 0.5/(g*chi) * (1/(g*sqrt(g*g-1)) - 1/chi) *
+    S1z/(sig * pow(a, 3));
+
+  dXdt[2] = (term1 + term2 + term3) / denom;  // = g'
 }
 
 void initialize_constants()
@@ -166,11 +170,6 @@ void load_polylog_table()
     ASSERT(n < MAXTABLE, "polylog table exceeded max table size %d", MAXTABLE);
   }
   PolylogTableN = n;
-
-  //  printf("ImpactParameter = %f\n", ImpactParameter);
-  //  printf("DimensionOfBrane = %d\n", DimensionOfBrane);
-  //  printf("Size of polylog table = %d\n", PolylogTableN);
-
   fclose(table);
 }
 
@@ -200,8 +199,8 @@ int main()
     ASSERT(IterationNumber < MAXITER, "integration exceeded maximum size %d",
            MAXITER);
 
-    double g = X[2];
-    double chi = atanh(sqrt(g*g - 1)/g);
+    //    double g = X[2];
+    //    double chi = atanh(sqrt(g*g - 1)/g);
     double Q = NumberOfFluxes - 2.0*X[1]/LengthOfLargeExtraDimensions;
 
     printf("%14.12e %14.12e %14.12e %14.12e %14.12e\n", X[0], X[1], X[2], X[3], Q);
@@ -222,87 +221,66 @@ int main()
  * Physics functions
  *
  */
+double Sx(double z, int which)
+{
+  int WindowSize = 100;
+  int i, i0, i1;
+
+  i1 = IterationNumber;
+  i0 = i1 - WindowSize;
+
+  if (i0 < 0) i0 = 0;
+
+  double *integrand = (double*) malloc((i1-i0) * sizeof(double));
+  double *times     = (double*) malloc((i1-i0) * sizeof(double));
+
+  for (i=i0; i<i1; ++i) {
+
+    double *Xi = TrajectoryRecord[i];
+    double ti = Xi[0];
+    double zi = Xi[1];
+    double gi = Xi[2];
+    double ai = Xi[3];
+
+    double K = 0.0; // sum of kernel over collision points
+    double zcol = 0.0; // z-coordinate of collision points
+
+    double L = LengthOfLargeExtraDimensions;
+    
+    while (zcol < z) {
+      zcol += 0.5 * L;
+      K += kernel_function(zcol - zi);
+    }
+
+    double ms   = StringMass;
+    int p       = DimensionOfBrane;
+    double dp   = pow(LengthOfSmallExtraDimensions, p-3); // also called Vwrap in da-code
+    double chi  = atanh(sqrt(gi*gi-1)/gi);
+    
+    if (which == 1) {
+      integrand[i-i0] = dp * pow(ms,p+2)/pow(2*PI, p) * pow(chi,0.5*p)* F_function(chi) * abs(z-zi) * pow(ai,3) * K;
+    }
+    else if (which == 2) {
+      integrand[i-i0] = dp * pow(ms,p+2)/pow(2*PI, p) * pow(chi,0.5*p)* F_function(chi) * abs(z-zi)/(z-zi) * pow(ai,3) * K;
+    }
+    times[i-i0] = ti;
+  }
+
+  double definite_integral =
+    numerically_integrate(times, integrand, i1-i0);
+  free(integrand);
+  free(times);
+
+  return definite_integral;  
+}
 
 double S1(double z)
 {
-  int i;
-  double *S1_integrand = (double*) malloc(IterationNumber * sizeof(double));
-  double *S1_time      = (double*) malloc(IterationNumber * sizeof(double));
-
-  //  printf("IterationNumber=%d\n", IterationNumber);
-
-  for (i=0; i<IterationNumber; ++i) {
-
-    double *Xi = TrajectoryRecord[i];
-    double ti = Xi[0];
-    double zi = Xi[1];
-    double gi = Xi[2];
-    double ai = Xi[3];
-
-    double K = 0.0; // sum of kernel over collision points
-    double zcol = 0.0; // z-coordinate of collision points
-
-    double L = LengthOfLargeExtraDimensions;
-    
-    while (zcol < z) {
-      zcol += 0.5 * L;
-      K += kernel_function(zcol - zi);
-    }
-
-    double ms   = StringMass;
-    int p       = DimensionOfBrane;
-    double dp   = pow(LengthOfSmallExtraDimensions, p-3); // also called Vwrap in da-code
-    double chi  = atanh(sqrt(gi*gi-1)/gi);
-    
-    S1_integrand[i] = dp * pow(ms,p+2)/pow(2*PI, p) * pow(chi,0.5*p)* F_function(chi) * abs(z-zi) * pow(ai,3) * K;
-    S1_time[i]      = ti;
-  }
-  double definite_integral =
-    numerically_integrate(S1_time, S1_integrand, IterationNumber);
-  free(S1_integrand);
-  free(S1_time);
-  return definite_integral;
-  
+  return Sx(z, 1);
 }
-
 double S2(double z)
 {
-  int i;
-  double *S2_integrand = (double*) malloc(IterationNumber * sizeof(double));
-  double *S2_time      = (double*) malloc(IterationNumber * sizeof(double));
-
-  for (i=0; i<IterationNumber; ++i) {
-
-    double *Xi = TrajectoryRecord[i];
-    double ti = Xi[0];
-    double zi = Xi[1];
-    double gi = Xi[2];
-    double ai = Xi[3];
-
-    double K = 0.0; // sum of kernel over collision points
-    double zcol = 0.0; // z-coordinate of collision points
-    
-    double L = LengthOfLargeExtraDimensions;
-
-    while (zcol < z) {
-      zcol += 0.5 * L;
-      K += kernel_function(zcol - zi);
-    }
-
-    double ms   = StringMass;
-    int p       = DimensionOfBrane;
-    double dp   = pow(LengthOfSmallExtraDimensions, p-3); // also called Vwrap in da-code
-    double chi  = atanh(sqrt(gi*gi-1)/gi);
-    
-    S2_integrand[i] = dp * pow(ms,p+2)/pow(2*PI, p) * pow(chi,0.5*p)* F_function(chi) * abs(z-zi)/(z-zi) * pow(ai,3) * K;
-    S2_time[i]      = ti;
-  }
-  double definite_integral =
-    numerically_integrate(S2_time, S2_integrand, IterationNumber);
-  free(S2_integrand);
-  free(S2_time);
-  return definite_integral;
-  
+  return Sx(z, 2);
 }
 
 double potential_function(double z)
